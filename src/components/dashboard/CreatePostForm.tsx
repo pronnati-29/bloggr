dfa943e7926c50e751cced02a46fd5885b8b99fb
pronnati prabha <pronnati32@gmail.com>
2026-09-13 23:54:06 +0530

    Update API URL to use environment variable

--- src/components/dashboard/CreatePostForm.tsx
+++ src/components/dashboard/CreatePostForm.tsx
@@ -26,7 +26,7 @@ export default function CreatePostForm() {
       return;
     }
     try {
-      const res = await fetch("http://localhost:5000/api/posts/create", {
+      const res = await fetch("process.env.NEXT_PUBLIC_API_URL/api/posts/create", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
